2915c9fbe4f84234d32d8c406512a7775311a390
rainysaturday <rainysaturday@github.com>
2024-11-03 09:43:54 +0100

    Basic go server
    Signed-off-by: rainysaturday <rainysaturday@github.com>

---
+++ server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+var request_counter = 0
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	size_param := r.URL.Query()["size"]
+	var size int64 = 0
+	if len(size_param) > 0 {
+		size_parsed, err := strconv.ParseInt(size_param[0], 10, 64)
+		if err == nil {
+			size = size_parsed
+		}
+	}
+
+	request_counter += 1
+	fmt.Println("Received request " + strconv.Itoa(request_counter) + " from " + r.Host + " with " + fmt.Sprintf("%d", size))
+
+	var i int64 = 0
+	for ; i < size; i++ {
+		fmt.Fprint(w, "Hello, World!")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	http.ListenAndServe(":8080", nil)
+}
